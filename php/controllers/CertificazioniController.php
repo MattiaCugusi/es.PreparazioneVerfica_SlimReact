bdf8962eb9cb83440e2294300caf8dba75a19a8a
Mattia Cugusi <m.cugusi287gmail.com>
2025-04-13 10:05:37 +0200

    es. completato

--- php/controllers/CertificazioniController.php
+++ php/controllers/CertificazioniController.php
@@ -47,5 +47,54 @@ Class CertificazioniController {
     
   }
 
+  public function update(Request $request, Response $response, $args)
+  {
+    $body=json_decode($request->getBody()->getContents(), true);
+    $id = $args["id_cert"];
+    $idAlunno = $body["idAlunno"];
+    $titolo = $body["titolo"];
+    $votazione = $body["votazione"];
+    $ente = $body["ente"];
+
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("UPDATE certificazioni SET ('$idAlunno', '$titolo', '$votazione', '$ente') WHERE id = " . $id);
+    if($mysqli_connection->affected_rows>0)
+    {
+      $results=["msg"=>"ok"];
+      $response->getBody()->write(json_encode($results));
+      return $response->withHeader("Content-type", "application/json")->withStatus(201);
+    }
+    $results=["msg"=>"ko"];
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(400);
+    
+  }
+
+  public function delete(Request $request, Response $response, $args)
+  {
+    $id = $args["id_cert"];
+
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("DELETE FROM certificazioni WHERE id = " . $id);
+
+    if($mysqli_connection->affected_rows>0)
+    {
+      $results=["msg"=>"ok"];
+      $response->getBody()->write(json_encode($results));
+      return $response->withHeader("Content-type", "application/json")->withStatus(201);
+    }
+    $results=["msg"=>"ko"];
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(400);
+    
+
+
+   
+  }
+
+
+
+
+
 
 }
